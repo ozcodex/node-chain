b56380756f5ac0b9b329cf755a799319d49edf8d
OscarE <oscare@debian>
2018-07-11 13:45:37 -0500

    Initial commit

---
+++ blockchain.js
@@ -0,0 +1,35 @@
+/**
+ * @class Blockchain
+ *
+ * @classdesc This class contain the functions to manage a blockchain over node
+ * inside it have two arrays with the chain and the nodes.
+ *
+ * @author      Oscar Eduardo Bernal <oscarbernal93@gmail.com>
+ *
+ * @requires    request-promise
+ * @requires    sha3
+ * @requires    fs
+ *
+ *
+ * @prop {Array}	chain		The entire blockchain with all the blocks
+ * @prop {Array}	nodes		The record of all the nodes
+ *  
+ */
+
+ class Ethereum {
+ 	constructor() {}
+ 	/**
+     * @function new_block
+     * @memberof Blockchain
+     *
+     * @desc This function 
+     *
+     * @param {proof}		proof of work number
+     * @param {string}      hash of the previous block
+     * 
+     * @return {boolean}	result of the operation    
+     */
+    new_block(proof, previous_hash) {
+    	
+    }
+}
